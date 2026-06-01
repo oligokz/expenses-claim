f697ade274bdbc78a2e879d102f909549cb6f0ab
oligokz <BERNARD.LIM@OUTLOOK.SG>
2026-06-02 02:43:36 +0800

    Fixed rendering

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Serve /index.html at the root / path
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ]
-  },
 }
-
 module.exports = nextConfig
